ca3bce48d5ec4d07e95d6ce7bbcbf75cd30fdea1
adititiwari0810 <aditiwari0810@gmail.com>
2025-02-01 01:43:51 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <h1>hello guys</h1>
+    <Button>start here</Button>
+    
+
+  </div>
+  );
+}
